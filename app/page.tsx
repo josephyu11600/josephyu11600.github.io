a00dc3e42fb9bbe5fbbaa23bb21a1c3f0da1353e
josephwyu <josephyu11600@berkeley.edu>
2024-06-16 02:18:06 -0700

    fix next version and updating media queries to use flex row on larger screens

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,23 @@
-import { BlogPosts } from 'app/components/posts'
+import { BlogPosts } from 'app/components/posts';
 
 export default function Page() {
-  return (
-    <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
-      </h1>
-      <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
+	return (
+		<section>
+			<h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+				My Portfolio
+			</h1>
+			<div className="flex flex-col md:flex-row">
+				<div className="flex-1">
+					<BlogPosts />
+				</div>
+				<p className="mb-4 flex-1 mr-5">
+					{`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
         Vim's keystroke commands and tabs' flexibility for personal viewing
         preferences. This extends to my support for static typing, where its
         early error detection ensures cleaner code, and my preference for dark
         mode, which eases long coding sessions by reducing eye strain.`}
-      </p>
-      <div className="my-8">
-        <BlogPosts />
-      </div>
-    </section>
-  )
+				</p>
+			</div>
+		</section>
+	);
 }
