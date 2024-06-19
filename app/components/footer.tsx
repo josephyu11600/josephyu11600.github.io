8ae287af3e8182be92d436a1394c71b132445fcc
josephwyu <josephyu11600@berkeley.edu>
2024-06-19 02:49:47 -0700

    removed unused components

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -24,17 +24,6 @@ export default function Footer() {
 						className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
 						rel="noopener noreferrer"
 						target="_blank"
-						href="/rss"
-					>
-						<ArrowIcon />
-						<p className="ml-2 h-7">rss</p>
-					</a>
-				</li>
-				<li>
-					<a
-						className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
-						rel="noopener noreferrer"
-						target="_blank"
 						href="https://github.com/vercel/next.js"
 					>
 						<ArrowIcon />
@@ -53,9 +42,6 @@ export default function Footer() {
 					</a>
 				</li>
 			</ul>
-			<p className="mt-8 text-neutral-600 dark:text-neutral-300">
-				© {new Date().getFullYear()} MIT Licensed
-			</p>
 		</footer>
 	);
 }
