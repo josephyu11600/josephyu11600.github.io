d0101fca70183506d1b8d61ffbe6960525ef9f32
josephwyu <josephyu11600@berkeley.edu>
2024-06-27 02:53:10 -0700

    portfolio picture with a new intro paragraph

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { BlogPosts } from 'app/components/posts';
 import DynamicHeader from './components/header';
+import portfolioImage from './portfolio_image.jpg';
 
 export default function Page() {
 	return (
@@ -8,15 +9,34 @@ export default function Page() {
 			<DynamicHeader />
 			<div className="flex flex-col md:flex-row">
 				<div className="flex-1">
-					<BlogPosts />
+					<img
+						className="h-auto max-w-2xs rounded-lg"
+						src={portfolioImage.src}
+					/>
 				</div>
-				<p className="mb-4 flex-1 mr-5">
-					{`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
-				</p>
+				<div className="mb-4 flex-1 mr-5">
+					<p className="mb-4">
+						{`
+              I am currently a first year Masters student at Georgia Tech studying Computer Science.
+              `}
+					</p>
+					<p className="mb-4">
+						{`I have industry experience building UI with`}{' '}
+						<span className="font-bold">{`React`} </span>{' '}
+						{`, services using `}{' '}
+						<span className="font-bold">{`Python`}</span> {`and`}{' '}
+						<span className="font-bold">{`Go`}</span>
+						{`, and leveraging `}
+						<span className="font-bold">{`Kubernetes`}</span>{' '}
+						{`and`} <span className="font-bold">{`Docker`}</span>{' '}
+						{`to deploy applications onto different cloud environments.`}
+					</p>
+					{`
+            I enjoy diving deep and moving quickly to tackle new features with high levels of ambiguity. In the process I find myself digging through documentation for containerized solutions, learning new frameworks, and collaborating with open source developers
+            (I've lost track of the number of late night threads I've started on their discords).
+          `}
+					<p></p>
+				</div>
 			</div>
 		</section>
 	);
