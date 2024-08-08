01f3f64c39539411f2db5c659f5b0322238352f7
josephwyu <josephyu11600@berkeley.edu>
2024-08-08 11:01:16 -0700

    add experience item component

---
+++ app/components/ExperienceItem.tsx
@@ -0,0 +1,31 @@
+import {
+	TimelineItem,
+	TimelinePoint,
+	TimelineContent,
+	TimelineTime,
+	TimelineTitle,
+	TimelineBody,
+} from 'flowbite-react';
+
+import { HiCalendar } from 'react-icons/hi';
+
+export type Experience = {
+	date: string;
+	title: string;
+	description: string;
+};
+
+export const ExperienceItem = (props: Experience): JSX.Element => {
+	const { date, title, description } = props;
+
+	return (
+		<TimelineItem key={title}>
+			<TimelinePoint />
+			<TimelineContent>
+				<TimelineTime>{date}</TimelineTime>
+				<TimelineTitle>{title}</TimelineTitle>
+				<TimelineBody>{description}</TimelineBody>
+			</TimelineContent>
+		</TimelineItem>
+	);
+};
---
+++ app/experience/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+export default function Page() {
+	return (
+		<section>
+			<h1 className="font-semibold text-2xl mb-8 tracking-tighter">
+				My Experiences
+			</h1>
+		</section>
+	);
+}
